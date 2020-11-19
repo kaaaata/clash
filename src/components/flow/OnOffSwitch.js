b05c68c2ad40ba122797af19808504f79d13c0a5
kaaaata <catherinehan714@gmail.com>
2020-11-18 20:25:43 -0800

    Separate on off button component

--- src/components/flow/OnOffSwitch.js
+++ src/components/flow/OnOffSwitch.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { css, jsx } from '@emotion/core'; /** @jsx jsx */
-import { colors } from '../styles';
 import { Text } from '../particles';
+import { OnOffButton } from './OnOffButton';
 
 const onOffSwitchCss = css`
   display: flex;
@@ -12,47 +12,6 @@ const onOffSwitchCss = css`
   .text {
     margin-right: 25px;
   }
-
-  .button {
-    width: 80px;
-    height: 40px;
-    margin: auto;
-    border-radius: 20px;
-    overflow: hidden;
-    display: inline-block;
-
-    div {
-      height: 100%;
-    }
-
-    .slider {
-      width: 160px;
-      position: relative;
-      transition: margin-left 0.1s ease-out;
-
-      .green {
-        position: absolute;
-        background: ${colors.green};
-        width: 50%;
-      }
-
-      .red {
-        position: absolute;
-        left: 50%;
-        background: ${colors.red};
-        width: 50%;
-      }
-      
-      .dot {
-        width: 40px;
-        background: ${colors.greyDark};
-        position: absolute;
-        left: 50%;
-        border-radius: 50%;
-        transform: translateX(-50%);
-      }
-    }
-  }
 `;
 
 export const OnOffSwitch = ({ name, description }) => {
@@ -61,17 +20,10 @@ export const OnOffSwitch = ({ name, description }) => {
   return (
     <div css={onOffSwitchCss}>
       <Text>{description}</Text>
-
-      <div className='button' onClick={() => {
+      <OnOffButton isOn={isOn} onClick={() => {
         window.localStorage.setItem(`clash_${name}`, JSON.stringify(!isOn));
         setIsOn(!isOn);
-      }}>
-        <div className='slider' style={{ marginLeft: isOn ? '-20px' : '-60px' }}>
-          <div className='green' />
-          <div className='red' />
-          <div className='dot' />
-        </div>
-      </div>
+      }} />
     </div>
   );
 };
