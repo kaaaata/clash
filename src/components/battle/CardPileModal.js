e86f88b8f213adca2d290a449b34ef4fddc8b963
kaaaata <catherinehan714@gmail.com>
2020-12-11 18:26:19 -0800

    Adjust card pile modal sorting algorithms

--- src/components/battle/CardPileModal.js
+++ src/components/battle/CardPileModal.js
@@ -1,13 +1,14 @@
 import { jsx } from '@emotion/core'; /** @jsx jsx */
-import { shuffle } from 'lodash';
 import { useSelector, useDispatch } from 'react-redux';
+import { cards } from '../../cards/cards';
+import { rarityScore } from '../../cards/rarity';
 import * as actions from '../../stores/actions';
 import { CardViewModal } from '../modals/CardViewModal';
 
 export const CardPileModal = () => {
-  const { activeModalCardPile, cards, enemyName } = useSelector(state => ({
+  const { activeModalCardPile, modalCards, enemyName } = useSelector(state => ({
     activeModalCardPile: state.clashBattleCards.activeModalCardPile,
-    cards: state.clashBattleCards.activeModalCardPile
+    modalCards: state.clashBattleCards.activeModalCardPile
       ? state.clashBattleCards[state.clashBattleCards.activeModalCardPile]
       : [],
     enemyName: state.clashBattleStats.enemyName
@@ -28,8 +29,8 @@ export const CardPileModal = () => {
     <CardViewModal
       title={cardPileModalNames[activeModalCardPile]}
       cardIds={activeModalCardPile === 'yourDeck'
-        ? shuffle(cards) :
-        cards.map(_ => _).reverse()
+        ? [...modalCards].sort((a, b) => rarityScore[cards[b].rarity] - rarityScore[cards[a].rarity])
+        : [...modalCards].reverse()
       }
       closeModal={() => dispatch(actions.setActiveModalCardPile(null))}
     />
